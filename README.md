742bbd7eeafc4252b4babae7255c80baec0ef2e3
CookieMr <dawid.boisse@gmail.com>
2023-01-08 16:53:21 +0100

    Publish to SQS every 2 seconds (scheduled)

---
+++ README.md
@@ -0,0 +1,29 @@
+# Publisher/Subscriber Java App with AWS SQS
+
+This is a simple project to use SQS in some manner.
+
+This project does one thing:
+* publishes a message to an AWS SQS
+
+### Running
+The AWS SQS must exists prior to running this app and the proper credentials
+need to be set in `~/.aws/credentials` file along with a region (for now
+it's hardcoded to be `us-west-2`).
+
+```bash
+mvn clean install && \
+mvn spring-boot:run
+```
+
+### AWS CLI - SQS
+```bash
+aws sqs get-queue-attributes \
+  --region us-west-2 \
+  --queue-url THE-QUEUE-URL \
+  --attribute-names ApproximateNumberOfMessages
+```
+```bash
+aws sqs purge-queue \
+  --region us-west-2 \
+  --queue-url THE-QUEUE-URL
+```
--- src/main/java/mr/cookie/config/SqsConfiguration.java
+++ src/main/java/mr/cookie/config/SqsConfiguration.java
@@ -1,10 +1,30 @@
 package mr.cookie.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
+import javax.annotation.PreDestroy;
+
+@Slf4j
 @Configuration
 public class SqsConfiguration {
 
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.US_WEST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
 
+    @PreDestroy
+    private void preDestroy() {
+        log.info("Closing SQS connection.");
+        sqsClient().close();
+    }
 
 }
